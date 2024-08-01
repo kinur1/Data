6535969d9bad4ab113ad1d1462fe1f0035ddf1aa
kinur1 <151740933+kinur1@users.noreply.github.com>
2024-08-02 06:34:59 +0700

    Update data.py

--- data.py
+++ data.py
@@ -5,16 +5,6 @@ import plotly.graph_objects as go
 
 st.title('Yahoo Finance Ticker Data Viewer')
 
-# Download the complete list of available tickers
-ticker_list = yf.Tickers('')
-
-# Extract ticker symbols and company names
-all_tickers = [(ticker, ticker_info.info['longName']) for ticker, ticker_info in ticker_list.tickers.items()]
-
-# Create a DataFrame for the data
-ticker_df = pd.DataFrame(all_tickers, columns=['Ticker', 'Company Name'])
-
-
 # User input for tickers
 ticker_input = st.text_input("Enter Ticker Symbols (comma-separated, e.g., TSLA, AAPL):", 'TSLA, AAPL')
 tickers = [ticker.strip().upper() for ticker in ticker_input.split(',')]
@@ -26,8 +16,14 @@ end_date = st.date_input("Select end date", pd.to_datetime('today'))
 # Download data for each ticker
 data = {}
 for ticker in tickers:
-    stock_data = yf.download(ticker, start=start_date, end=end_date)
-    data[ticker] = stock_data
+    try:
+        stock_data = yf.download(ticker, start=start_date, end=end_date)
+        if not stock_data.empty:
+            data[ticker] = stock_data
+        else:
+            st.warning(f"No data found for ticker: {ticker}")
+    except Exception as e:
+        st.error(f"Error downloading data for ticker: {ticker}. Error: {e}")
 
 # Display data in tables and candlestick charts
 for ticker, stock_data in data.items():
